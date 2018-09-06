ab0bca77b396dc5b807bb6eebb11f6f4237bc33e
Andy Tamez <AndyTamez@Andys-MacBook-Air.local>
2018-09-06 14:18:55 -0700

    add wireframe for HTML and CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$("#submitBtn").click(function(){
+
+    $("#containerOne").hide();
+    $("#containerTwo").show();
+
+});
+
+$("#submitButton").click(function(){
+
+    $("#containerOne").hide();
+    $("#containerTwo").hide();
+    $("#containerThree").show();
+});
