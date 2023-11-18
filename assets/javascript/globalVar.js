422ffd8aae4db76d8ddfbeb5fb6de68e1a61be3c
Dan Hsu <daniel.cw.hsu@gmail.com>
2023-11-18 15:05:23 -0800

    update cors proxy

--- assets/javascript/globalVar.js
+++ assets/javascript/globalVar.js
@@ -1,6 +1,6 @@
 let apiKeyGoogle = "AIzaSyAF1UgxUH_4QsFZUkqJI3_ks0LfHDBvY8o";
 let apiKeyMapquest = "YWon1ButUCpunKuVyxcVUnyjibTY4v2W";
-let corsAnywhereLink = "https://cors-anywhere.herokuapp.com/";
+let corsAnywhereLink = "https://corsproxy.io/?";
 let map;
 let directionService;
 let directionDisplay;
